83a4141a19f755ec40e09a5c795c6dd209c4a3d3
Mina Tadros <bobnakhla@gmail.com>
2024-06-23 21:07:41 -0400

    Initial commit

---
+++ src/main/java/me/empty/itemexchanger/ItemExchanger.java
@@ -0,0 +1,20 @@
+package me.empty.itemexchanger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ItemExchanger extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("Enabled!");
+
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("Disabled!");
+
+        // Plugin shutdown logic
+    }
+}
